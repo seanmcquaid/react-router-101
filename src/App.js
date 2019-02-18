ee36aebabb0502c71e06f5cdf6d26e77b89d807e
seanmcquaid <sean.mcquaid2@verizon.net>
2019-02-18 12:03:58 -0500

    started learning react router and link

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// add the react router module
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+
 
 class App extends Component {
   render() {
+    // The router goes around everything it needs to control
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+        <ul>
+          {/* in React router, we DONT USE ANCHOR TAGS */}
+          {/* That's so 2015..... */}
+          {/* Link component is the new anchor tag (<a>) */}
+          {/* <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li> */}
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+        </ul>
+        {/* Use the Route component to set up a path match*/}
+        {/* if the path matches the browser path, it will render */}
+        {/* whatever is given as component prop */}
+        {/* route is self closing */}
+        {/* if you have a path by itself, it will look for that path in the url */}
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+        </div>
+      </Router>
     );
   }
 }
